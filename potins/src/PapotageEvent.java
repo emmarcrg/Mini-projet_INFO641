498d3513e32206a3e4d3f960ea20560e7fd21bc8
Emma Rechon-Reguet <emma.rechon-reguet@inria.fr>
2024-05-13 14:11:28 +0200

    Classe PapotageEvent

--- potins/src/PapotageEvent.java
+++ potins/src/PapotageEvent.java
@@ -1,3 +1,14 @@
 public class PapotageEvent {
+    private String sujet;
+    private String corps;
 
+    public PapotageEvent(String sj, String cp){
+        this.sujet=sj;
+        this.corps=cp;
+    }
+
+    public String print_message (){
+        return "Le sujet "+ this.sujet+". Le message : "+this.corps;
+    }
 }
+
