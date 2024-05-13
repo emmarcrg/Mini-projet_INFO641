94955adc5ecf7cdbe1c9ff1e9953236b95f0e53a
Axelle <160240449+aquellery@users.noreply.github.com>
2024-05-13 15:34:27 +0200

    Ajout de la classe Batiment

---
+++ potins/src/Test.java
@@ -0,0 +1,25 @@
+public class Test {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Bienvenue dans le batiment où tous les potins sont permis");   
+
+        // Création du concierge
+        Concierge cyprien=new Concierge();
+
+        // Création du batiment
+        Batiment batiment = new Batiment("PotinLand", cyprien);
+
+        // Création de nouveaux bavards
+        Bavard charlotte=new Bavard(cyprien);
+        Bavard emma=new Bavard(cyprien);
+
+        // Ajout des bavard au batiment
+        batiment.ajouter_habitant(charlotte);
+        batiment.ajouter_habitant(emma);
+
+        batiment.souscription_potin();
+        
+        emma.transmettre_potin(new PapotageEvent("thomas", "pas de nouvelles"));
+
+    }
+
+}
