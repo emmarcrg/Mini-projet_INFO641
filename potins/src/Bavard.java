ef5833a2306c80fcbbd0c0b0ed4849df9c2c1855
Axelle <160240449+aquellery@users.noreply.github.com>
2024-05-13 14:21:29 +0200

    Création du Batiment

--- potins/src/Bavard.java
+++ potins/src/Bavard.java
@@ -1,3 +1,16 @@
 public class Bavard {
+    private Concierge concierge;
+
+    public Bavard(Concierge concierge){
+        this.concierge=concierge;
+    }
+
+    public Concierge get_concierge(){
+        return this.concierge;
+    }
+    
+    public void set_concierge(Concierge nouveau_concierge){
+        this.concierge=nouveau_concierge;
+    }
 
 }
