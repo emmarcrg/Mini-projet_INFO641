d86da1312ce801882358fe2ae73bf40e6bd3fe14
Emma Rechon-Reguet <emma.rechon-reguet@inria.fr>
2024-05-21 09:35:47 +0200

    Ajout de la classe OnLineBavardEvent : message si bavard en ligne

--- potins/src/Bavard.java
+++ potins/src/Bavard.java
@@ -39,7 +39,10 @@ public class Bavard implements PapotageListener {
     public void set_connection(Boolean etat_connection){
         this.connecte=etat_connection;
         if (etat_connection){
-        System.out.println("Le bavard "+this.get_nom()+" est connecté au potin");
+            System.out.println("Le bavard "+this.get_nom()+" est connecté au potin");
+            Object source = new Object();
+            OnLineBavardEvent ob= new OnLineBavardEvent(source, ActionEvent.ACTION_PERFORMED, "command",this);
+            ob.get_message();
         }
         else{
             System.out.println("Le bavard "+this.get_nom()+" n'est pas connecté au potin");
--- potins/src/OnLineBavardEvent.java
+++ potins/src/OnLineBavardEvent.java
@@ -1,11 +1,15 @@
 import java.awt.event.ActionEvent;
 
 public class OnLineBavardEvent extends ActionEvent{
-
+    private Bavard bavard;
     public OnLineBavardEvent(Object source, int id, String command, Bavard b) {
         super(source, id, command);
+        this.bavard=b;
         System.out.println("Le bavard "+ b.get_nom() +" est connecté." );
     }
 
+    public String get_message(){
+        return "Le bavard "+ this.bavard.get_nom() +" est connecté." ;
+    }
 
 }
